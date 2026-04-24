d754b78bbeac153958fecc6bfa17a6cc1c304b2a
Ankit Kumar <ankit.kumar@oniondev.com>
2026-04-24 15:29:50 +0530

    main URL

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,9 +1,9 @@
 const geoserverUrl = "https://geoserver.core-stack.org:8443/";
 const apiUrl = "https://geoserver.core-stack.org/api/v1/"; // for production
 // const apiUrl = "http://10.0.2.2:8080/api/v1/"; // for local development
-// const ccUrl = "https://nrm.core-stack.org/maps"; // for production
-const ccUrl = "https://master.d1rx3d0dyjc5v7.amplifyapp.com/maps"; // for uat
+const ccUrl = "https://nrm.core-stack.org/maps"; // for production
+// const ccUrl = "https://master.d1rx3d0dyjc5v7.amplifyapp.com/maps"; // for uat
 // const ccUrl ="http://192.168.29.33:5173/maps"; // for local development on android emulator
-// const ccUrl = "http://10.194.181.19:5173/maps"; // for local development on android device
+// const ccUrl = "http://192.168.1.11:5173/maps"; // for local development on android device
 
 const baseMapTileUrl = "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}";
